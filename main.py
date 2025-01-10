5aae914647abddb3c203723b961ad243b9714760
Андронов Олег <gennadych-65@yandex.ru>
2025-01-10 22:17:46 +0300

    add classes Bird, Mammal, Reptile
    Добавление подклассов, специфических атрибутов и методов.
    Переопределение метода make_sound

--- main.py
+++ main.py
@@ -11,3 +11,42 @@ class Animal:
 
     def eat(self):          # выводит, что животное ест
         return f"{self.name} ест."
+
+# 2. Реализуйте наследование, создав подклассы `Bird`, `Mammal`, и `Reptile`,
+# которые наследуют от класса `Animal`.
+# Добавьте специфические атрибуты и переопределите методы, если требуется
+# (например, различный звук для `make_sound()`).
+
+class Bird(Animal):
+    def __init__(self, name, age, flight_speed):
+        super().__init__(name, age)         # Вызов конструктора родительского класса
+        self.flight_speed = flight_speed    # Специфический атрибут - "скорость полета"
+
+    def make_sound(self):                   # Переопределение метода с учетом специфики класса
+        return "Фью-фью!"                   # Вывод звука, характерного для птиц
+
+    def fly(self):                          # Специфический метод - "летать"
+        return f"{self.name} летит со скоростью {self.flight_speed} м/сек."
+
+class Mammal(Animal):
+    def __init__(self, name, age, has_fur):
+        super().__init__(name, age)         # Вызов конструктора родительского класса
+        self.has_fur = has_fur              # Специфический атрибут - "наличие шерсти"
+
+    def make_sound(self):                   # Переопределение метода с учетом специфики класса
+        return "Р-р-р!"                     # Вывод звука, характерного для млекопитающих
+
+    def nurse(self):                        # Специфический метод - "вскармливание молоком"
+        return f"{self.name} кормит детеныша молоком."
+
+class Reptile(Animal):
+    def __init__(self, name, age, is_venomous):
+        super().__init__(name, age)         # Вызов конструктора родительского класса
+        self.is_venomous = is_venomous      # Специфический атрибут - "ядовитость"
+
+    def make_sound(self):                   # Переопределение метода с учетом специфики класса
+        return "Шшш!"                       # Вывод звука, характерного для рептилий
+
+    def slither(self):                      # Специфический метод - "ползать"
+        return f"{self.name} ползет по земле."
+
